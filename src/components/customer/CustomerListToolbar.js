89c631cfaddcdb88f5c68283d3bdcf138319b85c
Lucas Castro <lucas.sc98@hotmail.com>
2021-08-04 23:08:21 -0300

    Add state and behavior on form

--- src/components/customer/CustomerListToolbar.js
+++ src/components/customer/CustomerListToolbar.js
@@ -1,4 +1,5 @@
 // import { makeStyles } from '@material-ui/core/styles';
+import React, { useState } from 'react';
 import {
   Box,
   Card,
@@ -29,53 +30,70 @@ import {
 //   return classes;
 // }
 
-const CustomerListToolbar = (props) => (
-  <Box {...props}>
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'flex-end'
-      }}
-    >
-      <div>{}</div>
+const CustomerListToolbar = (props) => {
+  const [descricao, setDescricao] = useState('');
+  const [categoria, setCategoria] = useState('');
+
+  const submit = (event) => {
+    event.preventDefault();
+    console.log(`Valores: Descrição - ${descricao} e Categoria - ${categoria}`);
+  };
+
+  return (
+    <Box {...props}>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'flex-end'
+        }}
+      >
+        <div>{}</div>
+      </Box>
+      <Box sx={{ mt: 3 }}>
+        <Card>
+          <CardContent>
+            <Box>
+              <Grid container>
+                <Grid item md={4}>
+                  <FormControl fullWidth>
+                    <TextField
+                      fullWidth
+                      label="Descrição:"
+                      placeholder="Descrição da tarefa"
+                      variant="outlined"
+                      value={descricao}
+                      onChange={(e) => setDescricao(e.target.value)}
+                    />
+                  </FormControl>
+                </Grid>
+                <Grid item md={4}>
+                  <FormControl fullWidth>
+                    <InputLabel id="label">Categoria: </InputLabel>
+                    <Select
+                      labelId="label"
+                      id="select"
+                      value={categoria}
+                      onChange={(e) => setCategoria(e.target.value)}
+                    >
+                      <MenuItem value="">Selecione...</MenuItem>
+                      <MenuItem value="TRABALHO">Trabalho</MenuItem>
+                      <MenuItem value="ESTUDOS">Estudos</MenuItem>
+                      <MenuItem value="OUTROS">Outros</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+                <Grid item md={2}>
+                  <Button onClick={submit} variant="contained" color="primary">
+                    Adicionar
+                  </Button>
+                </Grid>
+              </Grid>
+            </Box>
+          </CardContent>
+        </Card>
+      </Box>
     </Box>
-    <Box sx={{ mt: 3 }}>
-      <Card>
-        <CardContent>
-          <Box>
-            <Grid container>
-              <Grid item md={4}>
-                <FormControl fullWidth>
-                  <TextField
-                    fullWidth
-                    label="Descrição:"
-                    placeholder="Descrição da tarefa"
-                    variant="outlined"
-                  />
-                </FormControl>
-              </Grid>
-              <Grid item md={4}>
-                <FormControl fullWidth>
-                  <InputLabel id="label">Categoria: </InputLabel>
-                  <Select labelId="label" id="select" value="">
-                    <MenuItem value="">Selecione...</MenuItem>
-                    <MenuItem value="TRABALHO">Trabalho</MenuItem>
-                    <MenuItem value="ESTUDOS">Estudos</MenuItem>
-                    <MenuItem value="OUTROS">Outros</MenuItem>
-                  </Select>
-                </FormControl>
-              </Grid>
-              <Grid item md={2}>
-                <Button variant="contained" color="secondary">
-                  Adicionar
-                </Button>
-              </Grid>
-            </Grid>
-          </Box>
-        </CardContent>
-      </Card>
-    </Box>
-  </Box>
-);
+  );
+};
 
 export default CustomerListToolbar;
