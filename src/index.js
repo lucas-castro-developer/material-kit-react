faadb8ae114d17c604ac9ca20ffd1be61f43a3f3
Lucas Castro <lucas.sc98@hotmail.com>
2021-08-12 19:06:30 -0300

    Redux Setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
-ReactDOM.render((
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+const store = applyMiddleware(thunk)(createStore)(mainReducer);
+
+ReactDOM.render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>
-), document.getElementById('root'));
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 
 serviceWorker.unregister();
