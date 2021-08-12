0ad920779cb4d5c82be1a6e083c52ab01eab046a
Lucas Castro <lucas.sc98@hotmail.com>
2021-08-12 19:28:30 -0300

    Action creator task list

--- src/store/tarefasReducer.js
+++ src/store/tarefasReducer.js
@@ -1,3 +1,9 @@
+import axios from 'axios';
+
+const http = axios.create({
+  baseURL: 'https://minhastarefas-api.herokuapp.com'
+});
+
 const ACTIONS = {
   LISTAR: 'TAREFAS_LISTAR',
   ADD: 'TAREFAS_ADD',
@@ -16,5 +22,20 @@ const tarefaReducer = (state = INITIAL_STATE, action) => {
       return state;
   }
 };
+
+export function listar() {
+  return (dispatch) => {
+    http
+      .get('/tarefas', {
+        headers: { 'x-tenant-id': localStorage.getItem('email_usuario_logado') }
+      })
+      .then((response) => {
+        dispatch({
+          type: ACTIONS.LISTAR,
+          tarefas: response.data
+        });
+      });
+  };
+}
 
 export default tarefaReducer;
